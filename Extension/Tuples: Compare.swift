c93759123192e11dde0d232d56a582aba7efa6c9
Phạm Quang Huy <pqhuy87it@gmail.com>
2019-06-13 11:19:57 +0900

    Update Tuples: Compare.swift

--- Extension/Tuples: Compare.swift
+++ Extension/Tuples: Compare.swift
@@ -16,3 +16,38 @@ func ==(lhs: MyEnum, rhs: MyEnum) -> Bool {
         return false
     }
 }
+
+enum CompassPoint: Equatable {
+    case North
+    case South
+    case East
+    case West
+}
+
+// Used to compare 'CompassPoint' enum
+func ==<T : Equatable>(lhs: T?, rhs: T?) -> Bool
+
+enum Barcode {
+    case UPCA(Int, Int)
+    case QRCode(String)
+    case None
+}
+
+extension Barcode: Equatable {
+}
+
+func ==(lhs: Barcode, rhs: Barcode) -> Bool {
+    switch (lhs, rhs) {
+    case (let .UPCA(codeA1, codeB1), let .UPCA(codeA2, codeB2)):
+        return codeA1 == codeA2 && codeB1 == codeB2
+
+    case (let .QRCode(code1), let .QRCode(code2)):
+        return code1 == code2
+
+    case (.None, .None):
+        return true
+
+    default:
+        return false
+    }
+}

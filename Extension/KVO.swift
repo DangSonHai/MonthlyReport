e031338c733f5ef60b25626f0f8784addf9b9257
Phạm Quang Huy <pqhuy87it@gmail.com>
2020-02-05 15:02:11 +0900

    Update KVO.swift

--- Extension/KVO.swift
+++ Extension/KVO.swift
@@ -24,3 +24,120 @@ class Hoge {
         // もしくは自身(Hoge)の解放
     }
 }
+
+
+extension ObjectUserDefaultable {
+    static func ovserve<Value>(_ type: Value.Type, forKey key: ObjectDefaultKey, changeHandler: @escaping UDChangeHandler<UserDefaults, Value>) -> UDKeyValueObservation {
+        let key = namespace(key)
+        return UserDefaults.standard.observe(type, forKey: key, changeHandler: changeHandler)
+    }
+}
+
+extension IntegerUserDefaultable { /* 同様の実装 */ }
+
+let observation = MyUserDefaults.observe(String.self, forKey: .userName) { defaults, change in
+    print(change)
+}
+// 型が固定のキーは上記の第一引数は無くても良い
+let observation2 = MyUserDefaults.observe(forKey: .needsBackup) { defaults, change in
+    print(change)
+}
+    
+protocol KeyValueObservationable {
+    // 外から見えるのはこれだけでよい
+    func invalidate()
+}
+
+extension NSKeyValueObservation: KeyValueObservationable {}
+extension UDKeyValueObservation: KeyValueObservationable {}
+
+// 使用例
+var observations = [KeyValueObservationable]()
+let observationNS = scrollView.observe(\.contentOffset) { ... }
+let observationUD = userDefaults.observe("userName") { ... }
+observations.append(contentsOf: [observationNS, observationUD])
+    
+class Hoge {
+    let defaults: UserDefaultable
+
+    init(defaults: UserDefaultable) {
+        self.defaults = defaults
+    }
+
+    func save(value: Int) {
+        defaults.set(value, forKey: "key")
+    }
+}
+
+// 通常時
+let hoge = Hoge(defaults: UserDefaults.standard)
+// テスト時
+let hoge = Hoge(defaults: UserDefaultsFake())
+    
+    
+public protocol UserDefaultable: UDKeyValueCodingAndObserving {
+    func object(forKey defaultName: String) -> Any?
+    func string(forKey defaultName: String) -> String?
+    ...
+
+    func set(_ value: Any?, forKey defaultName: String)
+    func set(_ value: Int, forKey defaultName: String)
+    ...
+
+    func removeObject(forKey defaultName: String)
+
+    // これだけ自前で用意
+    func reset()
+}
+    
+extension UserDefaults: UserDefaultable {
+    public func reset() {
+        guard let bundleId = Bundle.main.bundleIdentifier else { return }
+        removePersistentDomain(forName: bundleId)
+    }
+}
+    
+public class UserDefaultsFake: UserDefaultable {
+    private var values = [String: Any]() // 値格納用
+
+    // MARK: Getter --------------
+
+    func object(forKey defaultName: String) -> Any? {
+        return values[defaultName]
+    }
+
+    func string(forKey defaultName: String) -> String? {
+        return values[defaultName] as? String
+    }
+
+    ...
+
+    // MARK: Setter --------------
+
+    func set(_ value: Any?, forKey defaultName: String) {
+        guard let val = value else { removeObject(forKey: defaultName); return }
+        values[defaultName] = val
+    }
+
+    func set(_ value: Int, forKey defaultName: String) {
+        values[defaultName] = value
+    }
+
+    ...
+
+    func reset() {
+        values = [:]
+    }
+}
+    
+class Hoge {
+    let defaults: UserDefaultable
+
+    init(defaults: UserDefaultable) {
+        self.defaults = defaults
+    }
+
+    func save(value: Int) {
+        MyUserDefaults.set(value, forKey: .hogeKey, source: defaults)
+    }
+}
